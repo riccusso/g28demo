4eec5fb71e512b8a7bee210dc5f97c0591c60323
riccusso <tmtave@gmail.com>
2024-05-24 17:50:41 -0600

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Header from './components/Header'
 import Saludo from './components/Saludo'
 import Cards from './components/Cards'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import About from './pages/About'
+import Contacto from './pages/Contacto'
 
 function App() {
 
   return (
     <>
+    <Router>
       <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contacto'>Contacto</Link>
+          </li>
+        </ul>
+      </nav>
       <Saludo nombre={`Ricardo`} />
       <Cards />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/contacto' element={<Contacto />} />
+      </Routes>
+    </Router>
     </>
   )
 }
